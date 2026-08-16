8daf8263a10205a33232f47131e86faf4b5a70cf
Indrani <30498947+indraaani@users.noreply.github.com>
2026-08-16 16:14:44 -0400

    created enemy spawner

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject Enemy;
+    [SerializeField] private float spawnRate;
+    private float timer = 0;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+         SpawnEnemy();       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
+        }
+        else
+        {
+            SpawnEnemy();
+            timer = 0;
+        }
+        
+    }
+
+
+    void SpawnEnemy()
+    {
+        float spawnX = Random.Range
+                (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
+        float spawnY = Random.Range
+                (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y);
+
+
+       // Debug.Log("sup");
+       Instantiate(Enemy, new Vector3(Random.Range(spawnX, spawnY), Random.Range(spawnX, spawnY), 0), transform.rotation);
+    }
+}
