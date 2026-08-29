298d01c30e2415805ad6922b0e05d76d1bd2af2a
Indrani <30498947+indraaani@users.noreply.github.com>
2026-08-28 21:30:15 -0400

    removed useless redundant code

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -2,11 +2,10 @@ using UnityEngine;
 
 public class BulletBehaviour : MonoBehaviour
 {
-public GameObject bounds;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-         bounds = GameObject.FindGameObjectWithTag("Bounds");
+    
     }
 
     // Update is called once per frame
